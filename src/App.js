1abd9e39952c97936e1cc543c45341aef9150565
Sylvia Antons <67543909+sylviaantons@users.noreply.github.com>
2021-07-08 13:25:27 +0200

    linked and name

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+  <h1>Weather App</h1>
+  <footer>
+ Coded by Sylvia 
+  <a href="https://github.com/sylviaantons/react-app-weather"
+  target="_blank"
+  >
+  open sourced on Github
+  </a>
+ </footer>
+  </div>
   );
 }
-
-export default App;
