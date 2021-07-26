739dac57bb4a3afa7365ac315fab3b48b9941d3c
sylviaantons <sylvia@donchev.is>
2021-07-26 20:37:28 +0200

    error laga

--- src/WeatherForecast.jsx
+++ src/WeatherForecast.jsx
@@ -38,6 +38,8 @@ export default function WeatherForecast(props) {
                     <WeatherForecastDay data={dailyForecast} />
                   </div>
                 )
+              } else {
+                return null
               }
             })}
           </div>
